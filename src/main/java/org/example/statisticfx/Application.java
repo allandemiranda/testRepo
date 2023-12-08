b3033167cb708f39ac5924609b26e52c78a95680
Allan de Miranda Silva <allandemiranda@gmail.com>
2023-12-08 09:12:10 +0100

    init

--- src/main/java/org/example/statisticfx/Application.java
+++ src/main/java/org/example/statisticfx/Application.java
@@ -2,7 +2,6 @@ package org.example.statisticfx;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,9 +11,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 public class Application {
 
@@ -33,11 +33,15 @@ public class Application {
 
   public static void main(String[] args) {
 
-    final String inputFile = "C:\\Users\\allan\\OneDrive\\Documentos\\FX\\EURUSD_202306190007_202308182357.csv";
-    final String outputFolder = "C:\\Users\\allan\\OneDrive\\Documentos\\FX\\STATISTIC";
-    final String javaHome = "\"C:\\Program Files\\Java\\jdk-20.0.1\\bin\\java.exe\"";
-    final String classpath = "C:\\Users\\allan\\OneDrive\\Documentos\\FX\\forex-0.0.1-SNAPSHOT\\classes;C:\\Users\\allan\\OneDrive\\Documentos\\FX\\forex-0.0.1-SNAPSHOT\\lib\\*";
+//    final String inputFile = "C:\\Users\\allan\\OneDrive\\Documentos\\FX\\EURUSD_202306190007_202308182357.csv";
+//    final String outputFolder = "C:\\Users\\allan\\OneDrive\\Documentos\\FX\\STATISTIC";
+//    final String javaHome = "\"C:\\Program Files\\Java\\jdk-20.0.1\\bin\\java.exe\"";
+//    final String classpath = "C:\\Users\\allan\\OneDrive\\Documentos\\FX\\forex-0.0.1-SNAPSHOT\\classes;C:\\Users\\allan\\OneDrive\\Documentos\\FX\\forex-0.0.1-SNAPSHOT\\lib\\*";
 
+    final String inputFile = args[1];
+    final String outputFolder = args[2];
+    final String javaHome = args[3];
+    final String classpath = args[4];
     System.out.println("Generating scenarios");
 
     final LocalTime zeroTime = LocalTime.of(0, 0, 0);
@@ -66,16 +70,16 @@ public class Application {
 //            .flatMap(tp -> Arrays.stream(slM5).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingM5).map(t -> new Object[]{M_5, os, s, tp, sl, t, EMPTY, EMPTY})))))
 //        .toList();
 //
-//    //M15
-//    String[] onlyStrongM15 = new String[]{TRUE, FALSE};
-//    Integer[] spreadM15 = new Integer[]{5, 10, 12, 15};
-//    Integer[] tpM15 = new Integer[]{50, 70, 90, 100, 120, 150};
-//    Integer[] slM15 = new Integer[]{20, 50, 100};
-//    Integer[] tradingM15 = new Integer[]{-1, 50, 100};
-//
-//    final List<Object[]> parametersListM15 = Arrays.stream(onlyStrongM15).flatMap(os -> Arrays.stream(spreadM15).flatMap(s -> Arrays.stream(tpM15)
-//            .flatMap(tp -> Arrays.stream(slM15).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingM15).map(t -> new Object[]{M_15, os, s, tp, sl, t, EMPTY, EMPTY})))))
-//        .toList();
+    //M15
+    String[] onlyStrongM15 = new String[]{TRUE, FALSE};
+    Integer[] spreadM15 = new Integer[]{5, 10, 12, 15};
+    Integer[] tpM15 = new Integer[]{50, 70, 90, 100, 120, 150};
+    Integer[] slM15 = new Integer[]{20, 50, 100};
+    Integer[] tradingM15 = new Integer[]{-1, 50, 100};
+
+    final List<Object[]> parametersListM15 = Arrays.stream(onlyStrongM15).flatMap(os -> Arrays.stream(spreadM15).flatMap(s -> Arrays.stream(tpM15)
+            .flatMap(tp -> Arrays.stream(slM15).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingM15).map(t -> new Object[]{M_15, os, s, tp, sl, t, EMPTY, EMPTY})))))
+        .toList();
 
     //M30
     String[] onlyStrongM30 = new String[]{TRUE, FALSE};
@@ -87,48 +91,48 @@ public class Application {
     final List<Object[]> parametersListM30 = Arrays.stream(onlyStrongM30).flatMap(os -> Arrays.stream(spreadM30).flatMap(s -> Arrays.stream(tpM30)
             .flatMap(tp -> Arrays.stream(slM30).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingM30).map(t -> new Object[]{M_30, os, s, tp, sl, t, EMPTY, EMPTY})))))
         .toList();
-//
-//    //H1
-//    String[] onlyStrongH1 = new String[]{TRUE, FALSE};
-//    Integer[] spreadH1 = new Integer[]{5, 10, 12, 15};
-//    Integer[] tpH1 = new Integer[]{50, 100, 150, 200, 250, 500};
-//    Integer[] slH1 = new Integer[]{20, 50, 100, 150, 200, 300};
-//    Integer[] tradingH1 = new Integer[]{-1, 50, 100};
-//
-//    final List<Object[]> parametersListH1 = Arrays.stream(onlyStrongH1).flatMap(os -> Arrays.stream(spreadH1).flatMap(s -> Arrays.stream(tpH1)
-//            .flatMap(tp -> Arrays.stream(slH1).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingH1).map(t -> new Object[]{H_1, os, s, tp, sl, t, EMPTY, EMPTY})))))
-//        .toList();
-//
-//    //H2
-//    String[] onlyStrongH2 = new String[]{TRUE, FALSE};
-//    Integer[] spreadH2 = new Integer[]{5, 10, 12, 15};
-//    Integer[] tpH2 = new Integer[]{50, 100, 150, 200, 250, 500};
-//    Integer[] slH2 = new Integer[]{20, 50, 100, 150, 200, 300};
-//    Integer[] tradingH2 = new Integer[]{-1, 50, 100};
-//
-//    final List<Object[]> parametersListH2 = Arrays.stream(onlyStrongH2).flatMap(os -> Arrays.stream(spreadH2).flatMap(s -> Arrays.stream(tpH2)
-//            .flatMap(tp -> Arrays.stream(slH2).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingH2).map(t -> new Object[]{H_2, os, s, tp, sl, t, EMPTY, EMPTY})))))
-//        .toList();
-//
-//    //D1
-//    String[] onlyStrongD1 = new String[]{TRUE, FALSE};
-//    Integer[] spreadD1 = new Integer[]{5, 10, 12, 15};
-//    Integer[] tpD1 = new Integer[]{50, 100, 150, 200, 250, 500};
-//    Integer[] slD1 = new Integer[]{20, 50, 100, 150, 200, 300};
-//    Integer[] tradingD1 = new Integer[]{-1, 50, 100};
-//
-//    final List<Object[]> parametersListD1 = Arrays.stream(onlyStrongD1).flatMap(os -> Arrays.stream(spreadD1).flatMap(s -> Arrays.stream(tpD1)
-//            .flatMap(tp -> Arrays.stream(slD1).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingD1).map(t -> new Object[]{D_1, os, s, tp, sl, t, EMPTY, EMPTY})))))
-//        .toList();
+
+    //H1
+    String[] onlyStrongH1 = new String[]{TRUE, FALSE};
+    Integer[] spreadH1 = new Integer[]{5, 10, 12, 15};
+    Integer[] tpH1 = new Integer[]{50, 100, 150, 200, 250, 500};
+    Integer[] slH1 = new Integer[]{20, 50, 100, 150, 200, 300};
+    Integer[] tradingH1 = new Integer[]{-1, 50, 100};
+
+    final List<Object[]> parametersListH1 = Arrays.stream(onlyStrongH1).flatMap(os -> Arrays.stream(spreadH1).flatMap(s -> Arrays.stream(tpH1)
+            .flatMap(tp -> Arrays.stream(slH1).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingH1).map(t -> new Object[]{H_1, os, s, tp, sl, t, EMPTY, EMPTY})))))
+        .toList();
+
+    //H2
+    String[] onlyStrongH2 = new String[]{TRUE, FALSE};
+    Integer[] spreadH2 = new Integer[]{5, 10, 12, 15};
+    Integer[] tpH2 = new Integer[]{50, 100, 150, 200, 250, 500};
+    Integer[] slH2 = new Integer[]{20, 50, 100, 150, 200, 300};
+    Integer[] tradingH2 = new Integer[]{-1, 50, 100};
+
+    final List<Object[]> parametersListH2 = Arrays.stream(onlyStrongH2).flatMap(os -> Arrays.stream(spreadH2).flatMap(s -> Arrays.stream(tpH2)
+            .flatMap(tp -> Arrays.stream(slH2).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingH2).map(t -> new Object[]{H_2, os, s, tp, sl, t, EMPTY, EMPTY})))))
+        .toList();
+
+    //D1
+    String[] onlyStrongD1 = new String[]{TRUE, FALSE};
+    Integer[] spreadD1 = new Integer[]{5, 10, 12, 15};
+    Integer[] tpD1 = new Integer[]{50, 100, 150, 200, 250, 500};
+    Integer[] slD1 = new Integer[]{20, 50, 100, 150, 200, 300};
+    Integer[] tradingD1 = new Integer[]{-1, 50, 100};
+
+    final List<Object[]> parametersListD1 = Arrays.stream(onlyStrongD1).flatMap(os -> Arrays.stream(spreadD1).flatMap(s -> Arrays.stream(tpD1)
+            .flatMap(tp -> Arrays.stream(slD1).filter(sl -> sl < tp).flatMap(sl -> Arrays.stream(tradingD1).map(t -> new Object[]{D_1, os, s, tp, sl, t, EMPTY, EMPTY})))))
+        .toList();
 
     final List<Object[]> parameters = new ArrayList<>();
 //    parameters.addAll(parametersListM1);
 //    parameters.addAll(parametersListM5);
-//    parameters.addAll(parametersListM15);
     parameters.addAll(parametersListM30);
-//    parameters.addAll(parametersListH1);
-//    parameters.addAll(parametersListH2);
-//    parameters.addAll(parametersListD1);
+    parameters.addAll(parametersListH1);
+    parameters.addAll(parametersListM15);
+    parameters.addAll(parametersListH2);
+    parameters.addAll(parametersListD1);
     final List<Object[]> listTime = IntStream.range(0, startTime.size())
         .mapToObj(i -> new SimpleEntry<>(startTime.get(i).format(DateTimeFormatter.ISO_TIME), endTime.get(i).format(DateTimeFormatter.ISO_TIME))).flatMap(
             time -> parameters.stream()
@@ -169,19 +173,21 @@ public class Application {
     System.out.println("Merging files...");
 
     File folder = new File(outputFolder);
-    File endFile = new File(outputFolder, "allStatictic.csv");
-    try (final FileWriter fileWriter = new FileWriter(endFile, false)) {
+    File endFile = new File(outputFolder, "allStatistic.csv");
+
+    try (final FileWriter fileWriter = new FileWriter(endFile)) {
+      fileWriter.write(
+          "*TIME FRAME\t*SLOT OPEN\t*TP\t*SL\t*MAX SPREAD\t*MIN TRADING\t*ONLY STRONG\tWIN %\tTOTAL POSITION\tCONSISTENCE %\tNUMBER OF BAR\tLOW POINT\tHIGH POINT\n");
       Arrays.stream(folder.listFiles()).map(file -> {
         try (final FileReader fileReader = new FileReader(file); final BufferedReader bufferreader = new BufferedReader(fileReader)) {
-          return bufferreader.readLine();
-        } catch (FileNotFoundException e) {
-          throw new RuntimeException(e);
+          return StreamSupport.stream(bufferreader.lines().spliterator(), false).toList();
         } catch (IOException e) {
+
           throw new RuntimeException(e);
         }
-      }).forEachOrdered(line -> {
+      }).flatMap(Collection::stream).filter(s -> !(s.isBlank() || s.isEmpty())).forEach(line -> {
         try {
-          fileWriter.write(line);
+          fileWriter.write(line.concat("\n"));
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -189,7 +195,6 @@ public class Application {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-
     System.out.println("END!");
   }
 }
